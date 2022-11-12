befa894054137a3fd0d04678add965591a16420f
Bridger Hackworth <hac21003@byui.edu>
2022-11-11 20:57:49 -0700

    cleaned the city data

--- OnsemiFootprint/FootprintApp/app.R
+++ OnsemiFootprint/FootprintApp/app.R
@@ -1,14 +1,13 @@
 # install.packages(c( "gapminder", "ggforce", "gh", "globals", "openintro", "profvis", "RSQLite", "shiny", "shinycssloaders", "shinyFeedback",  "shinythemes", "testthat", "thematic", "tidyverse", "vroom", "waiter", "xml2", "zeallot" ))
 
 # TO DO:
-# Move everything to quarto in VS code
-# add reactive events to filter to only selected location
-# add in Onsemi logo title
-# add in an annotion to highlight the location
-# better looking markers on the map
-# add dropdown for different groups
-# add dropdown for different site functions
-# add in clickalble links to sharepoint site
+# add reactive events to filter to only selected location ✓
+# add in Onsemi logo title ✓
+# add in an annotion to highlight the location ✓
+# better looking markers on the map ✓
+# add dropdown for different groups ✓
+# add dropdown for different site functions ✓
+# add in clickalble links to sharepoint site ✓
 # Spice up the page more
 # fix city names, use code from colab
 library(tidyverse)
@@ -19,31 +18,29 @@ library(rnaturalearthdata)
 library(shiny)
 library(ggrepel)
 
-onsemi <- read_csv('./www/onsemi3.csv')
-
+# read in the data that is found in the same www file
+onsemi <- read_csv('./www/onsemi5.csv')
+# creating a separate tibble that has the links for the different groups 
 groupLinks <- tibble(cgroup = c('Advanced Solutions Group', 'Power Solutions Group', 'Intelligent Solutions Group')) %>% 
   mutate(link = case_when(cgroup == 'Advanced Solutions Group'~'https://onsemi.sharepoint.com/sites/asg', 
                           cgroup == 'Power Solutions Group'~'https://onsemi.sharepoint.com/sites/psg', cgroup =='Intelligent Solutions Group'~'https://onsemi.sharepoint.com/sites/isg')
   )
-
-
-
-world <- ne_countries(scale = "medium", returnclass = "sf")
-
-outline = 'white'
-
+world <- ne_countries(scale = "medium", returnclass = "sf") # this loads in the map of the world
+outline = 'white' # the point outlines, made this a variable to only have to change in one spot
+# this is the basic world map plot with all of the points included to serve as the default
 master <- ggplot(data = world) +
-  geom_sf(color = "black", fill = rgb(144, 238, 144, maxColorValue = 255)) +
+  geom_sf(color = "black", fill = rgb(144, 238, 144, maxColorValue = 255)) + #black outline, green earth
   labs(shape="Group", color = "Site Function") +
-  theme(panel.background = element_rect(fill = rgb(137, 207, 240, maxColorValue = 255)),
-        panel.grid.major = element_line(linetype = 'dashed', size = .3),
-        plot.title = element_text(hjust = 0.5), axis.title.x=element_blank(), axis.title.y=element_blank(), 
-        legend.position = "none") + 
-  scale_fill_manual(values = c('ATO' = rgb(255, 172, 28, maxColorValue = 255), 'other'=rgb(238, 75, 43, maxColorValue = 255), 
-                               'Wafer Fab Site' = rgb(34, 139, 34, maxColorValue = 255))) +
-  scale_shape_manual(values = c('ASG' = 21, 'non-ASG'= 24)) + 
-  geom_point(data = onsemi, aes(Longitude, Latitude, fill = site_type, shape=cgroup), color = outline, size = 5)
-
+  theme(panel.background = element_rect(fill = rgb(137, 207, 240, maxColorValue = 255)), # blue ocean
+        panel.grid.major = element_line(linetype = 'dashed', size = .3), # dashed gridlines
+        plot.title = element_text(hjust = 0.5), axis.title.x=element_blank(), 
+        axis.title.y=element_blank(), legend.position = "none") + #remove all titles and labels
+  scale_fill_manual(values = c('ATO' = rgb(255, 172, 28, maxColorValue = 255), 
+                               'other'=rgb(238, 75, 43, maxColorValue = 255), 
+                               'Wafer Fab Site' = rgb(34, 139, 34, maxColorValue = 255))) + # map specific colors to site types
+  scale_shape_manual(values = c('ASG' = 21, 'non-ASG'= 24)) + # map specific shapes to groups
+  geom_point(data = onsemi, aes(Longitude, Latitude, fill = site_type, shape=cgroup), color = outline, size = 5) # add in points
+# this is the base world map with no points to be layered over by the various plots, same code as master but without geompoint
 junior <- ggplot(data = world) +
   geom_sf(color = "black", fill = rgb(144, 238, 144, maxColorValue = 255)) +
   labs(shape="Group", color = "Site Function") +
@@ -55,40 +52,38 @@ junior <- ggplot(data = world) +
                                'Wafer Fab Site' = rgb(34, 139, 34, maxColorValue = 255))) +
   scale_shape_manual(values = c('ASG' = 21, 'non-ASG'= 24))
 
-outline = 'white'
-
 ui <- fluidPage(
   #includeScript("clickhandler.js"),
-  titlePanel(imageOutput("logo", height = '50px')),
+  titlePanel(imageOutput("logo", height = '50px')), #Onsemi Worldwide title 
   tabsetPanel(
     tabPanel('Interactive',
              fluidRow(
                column(9,
                       #plotlyOutput("plot", width = "900px", height = '450px'), 
                       #tags$head(tags$script(src="clickhandler.js")) # https://stackoverflow.com/questions/32057164/adding-hyperlinks-to-shiny-plots
-                      plotlyOutput("plot", width = "900px", height = '450px'), # make this reactive to city or group
+                      plotlyOutput("plot", width = "900px", height = '450px'), # main plotly plot
                       #imageOutput("homepage"),
                       #tags$a(imageOutput("homepage"),href="https://www.google.com")
-                      uiOutput("sharepoint")
+                      uiOutput("sharepoint") # link that will change based on drop down filter
                ), 
                column(2,
-                      selectInput("group", label = "Groups", selected = 'All', choices = append(c('All'), sort(groupLinks$cgroup))),
-                      selectInput("type", label = "Site Function", selected = 'All', choices = append(c('All'), sort(onsemi$site_type))),
-                      selectInput("city", label = "Find One Location", selected = 'All', choices = append(c('All'), sort(onsemi$city))),
-                      imageOutput("legend")
+                      selectInput("group", label = "Groups", selected = 'All', choices = append(c('All'), sort(groupLinks$cgroup))), # Dropdown menu
+                      selectInput("type", label = "Site Function", selected = 'All', choices = append(c('All'), sort(onsemi$site_type))), # Dropdown menu 
+                      selectInput("city", label = "Find One Location", selected = 'All', choices = append(c('All'), sort(onsemi$city))), # Dropdown menu
+                      imageOutput("legend") # load in image of ggplot legend because the plotly one is all buggy
                )
              )
     ), tabPanel('Static',
                 fluidRow(
                   column(9,
-                         plotOutput("plot2", width = "900px", height = '450px'), 
-                         uiOutput("sharepoint2")
+                         plotOutput("plot2", width = "900px", height = '450px'), # static plot that runs faster
+                         uiOutput("sharepoint2") # link that corrsponds with static plot filters and changes
                   ), 
-                  column(2,
+                  column(2, # dropdown menus, same as interactive plot ones
                          selectInput("group2", label = "Groups", selected = 'All', choices = append(c('All'), sort(groupLinks$cgroup))),
                          selectInput("type2", label = "Site Function", selected = 'All', choices = append(c('All'), sort(onsemi$site_type))),
                          selectInput("city2", label = "Find One Location", selected = 'All', choices = append(c('All'), sort(onsemi$city))),
-                         imageOutput("legend2")
+                         imageOutput("legend2") # static plot legend, same as interactive
                   )
                 )
     )
@@ -97,31 +92,31 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   output$plot <- renderPlotly( 
-    if (input$city != 'All') {
+    if (input$city != 'All') { # finding a single location overrides other dropdown menu filters
       ggplotly(junior + geom_point(data = onsemi %>% filter(city == input$city), 
                                    aes(Longitude, Latitude, fill = site_type, shape=cgroup, 
                                        text = paste0(city, ', ', country, '\n', site_type, '\n', cgroup)), 
                                    color = outline, size = 3), tooltip = 'text')
-    } else if (input$type != 'All' & input$group == 'All' & input$city == 'All') {
+    } else if (input$type != 'All' & input$group == 'All' & input$city == 'All') { # filtering the types only
       ggplotly(junior + geom_point(data = onsemi %>% filter(site_type == input$type), 
                                    aes(Longitude, Latitude, fill = site_type, shape=cgroup, 
                                        text = paste0(city, ', ', country, '\n', site_type, '\n', cgroup)), 
                                    color = outline, size = 3), tooltip = 'text')
-    } else if (input$group != 'All' & input$type == 'All' & input$city == 'All') {
-      tryCatch({
+    } else if (input$group != 'All' & input$type == 'All' & input$city == 'All') { # filtering the groups only
+      tryCatch({ # avoid erros if there is no data for a group, blank globe will show
       ggplotly(junior + geom_point(data = onsemi %>% filter(get(input$group) == 1), 
                                    aes(Longitude, Latitude, fill = site_type, shape=cgroup, 
                                        text = paste0(city, ', ', country, '\n', site_type, '\n', cgroup)), 
                                    color = outline, size = 3), tooltip = 'text')
       },error = function(e) {junior})
-    } else if (input$group != 'All' & input$type != 'All' & input$city == 'All') {
-      tryCatch({
+    } else if (input$group != 'All' & input$type != 'All' & input$city == 'All') { # filtering type and group
+      tryCatch({ # avoid errors in case of no data, show blank globe
         ggplotly(junior + geom_point(data = onsemi %>% filter(get(input$group) == 1 & site_type == input$type), 
                                      aes(Longitude, Latitude, fill = site_type, shape=cgroup, 
                                          text = paste0(city, ', ', country, '\n', site_type, '\n', cgroup)), 
                                      color = outline, size = 3), tooltip = 'text')
       },error = function(e) {junior})
-    } else {
+    } else { # default for when no filters have been entered
       ggplotly(junior + geom_point(data = onsemi, 
                                    aes(Longitude, Latitude, fill = site_type, shape=cgroup, 
                                        text = paste0(city, ', ', country, '\n', site_type, '\n', cgroup)), 
@@ -139,6 +134,7 @@ server <- function(input, output, session) {
     )
   }, deleteFile = FALSE)
   
+  # code is the same as for interactive, just without ggplotly function
   output$plot2 <- renderPlot( 
     if (input$city2 != 'All') {
       junior + geom_point(data = onsemi %>% filter(city == input$city2), 
@@ -187,6 +183,7 @@ server <- function(input, output, session) {
     )
   }, deleteFile = FALSE)
   
+  # not used
   output$homepage <- renderImage({
     list(
       src = file.path("./www/homepage.png"),
@@ -196,8 +193,7 @@ server <- function(input, output, session) {
     )
   }, deleteFile = FALSE)
 
-  
-  output$sharepoint <- renderUI({
+  output$sharepoint <- renderUI({ # these links, one for static and one for interactive, change based on the filter that is applied to the map
     if (input$city != 'All' & !is.na(filter(onsemi, city==input$city)[['link']][1])) {
       url <- a(paste0(input$city, " SharePoint Homepage"), href=filter(onsemi, city==input$city)[['link']][1])
     } else if (input$city == 'All' & input$group != 'All') {
--- OnsemiFootprint/wrangle3.R
+++ OnsemiFootprint/wrangle3.R
@@ -1,8 +1,34 @@
 #adding in links
 
-onsemi <- read_csv('https://raw.githubusercontent.com/bridg3r/Onsemi/main/OnsemiFootprint/onsemi2.csv')
+onsemi <- read_csv('/Users/bridg3r/Documents/GitHub/Onsemi/OnsemiFootprint/onsemi4.csv')
 
-onsemi <- onsemi %>% mutate(`Advanced Solutions Group` = case_when(cgroup == 'ASG'~1), `Power Solutions Group` = 0, `Intelligent Solutions Group` = 0)
+onsemi <- onsemi %>% select('Longitude', 'Latitude', 'Number_of_reviews', 'Review_Points', 'Address', 'city', 'country', 'site_type', 'cgroup')
+#Fixing the cities that got pulled out incorrectly, this could maybe be done with simpler 
+#code by making a list of the Asian countries and then pulling then splititng the address by commas
+# then if the country is an asian one, pulling the 3rd to last element in the split for city name
+onsemi <- onsemi %>% 
+  mutate(cgroup = ifelse(site_type != 'other', 'ASG', 'non-ASG'), 
+         city = case_when(Address=='Einsteinring 28, 85609 Aschheim, Germany'~'Aschheim',
+                          city== "#145"~ "Austin",
+                          Address =='Isafjordsgatan 32 C, 6tr, 164 40 Kista, Sweden'~'Kista',
+                          Address == 'Av. des Champs-Montants 12A, 2074 Marin-Epagnier, Switzerland'~'Marin-Epagnier',
+                          Address == 'Via Brescia, 26, 20063 Cernusco sul Naviglio MI, Italy'~'Cernusco sul Naviglio',
+                          Address == '2nd Floor, Greenwood House, London Rd, Bracknell RG12 2AA, United Kingdom'~'Bracknell',
+                          Address == "The Loughmore Centre, Loughmore Avenue, Raheen Business Park, Limerick, Ireland" ~'Limerick',
+                          Address =='Bulevardul Iuliu Maniu 6Q, București 061103, Romania'~'București',
+                          Address =='B-04-25, Krystal Point, 303, Jalan Sultan Azlan Shah, Krystal Point Corporate Park, 11900 Bayan Lepas, Pulau Pinang, Malaysia'~'Bayan Lepas',
+                          Address =='2 Commerce Ave, Alabang, Muntinlupa, Metro Manila, Philippines'~'Muntinlupa',
+                          Address =='Lorong Senawang 2/2, Kawasan Perusahaan Senawang, 70450 Senawang, Negeri Sembilan, Malaysia'~'Senawang',
+                          Address =='Lot 1.02 Level One, KPMG Tower, 8, First Ave, Bandar Utama, 47800 Petaling Jaya, Selangor, Malaysia'~'Petaling Jaya',
+                          Address =='2 Commerce Ave, Alabang, Muntinlupa, Metro Manila, Philippines'~'Muntinlupa',
+                          city == 'Ho'~'Hong Kong',
+                          city == 'Tarlac' ~ 'Tarlac City',
+                          Address == 'Số 10 Đường Số 17A, Bình Trị Đông B, Bình Tân, Thành phố Hồ Chí Minh, Vietnam'~'Ho Chi Minh City',
+                          city == 'Mala'~'Senawang',
+                          city == 'Pen'~'Singapore',
+                          TRUE ~ city)) 
+
+onsemi <- onsemi %>% mutate(`Advanced Solutions Group` = case_when(cgroup == 'ASG'~1, cgroup != 'ASG'~0), `Power Solutions Group` = 0, `Intelligent Solutions Group` = 0)
 
 groupLinks <- tibble(cgroup = c('Advanced Solutions Group', 'Power Solutions Group', 'Intelligent Solutions Group')) %>% 
   mutate(link = case_when(cgroup == 'Advanced Solutions Group'~'https://onsemi.sharepoint.com/sites/asg', 
@@ -29,5 +55,6 @@ onsemi <- onsemi %>% mutate(link = case_when(
   city =="Aizu"~ 'https://onsemi.sharepoint.com/sites/mfg-AFSM/SitePages/Home.aspx'
 ))
 
-write_csv(onsemi, "~/Documents/GitHub/Onsemi/OnsemiFootprint/onsemi3.csv")
+
+write_csv(onsemi, "/Users/bridg3r/Documents/GitHub/Onsemi/OnsemiFootprint/FootprintApp/www/onsemi5.csv")
 
